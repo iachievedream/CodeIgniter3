f591e23d2ac9c47431ac727e09ebfcf436c491f2
iachievedream <iachievedream@gmail.com>
2020-10-21 18:48:19 +0800

    deal index.php use add .htaccess file and db,routes,loginpage

---
+++ application/controllers/Login.php
@@ -0,0 +1,65 @@
+<?php if (!defined('BASEPATH')) {
+	exit('No direct script access allowed');
+}
+class Login extends CI_Controller {
+	public function __construct() {
+		parent::__construct();
+	}
+
+	public function index() {
+		$this->load->view('loginpage');
+	}
+
+	public function sign_in() {
+		$account = $this->input->post('account');
+		$password = $this->input->post('password');
+		if (!empty($account) and !empty($password)) {
+			$sql = 'SELECT * FROM  user WHERE account=? and password=?';
+			$q = $this->db->query($sql, [$account, md5($password)]);
+			if ($q->num_rows() > 0) {
+				//   $r = $q->first_row('array');
+				$r = true;
+			} else {
+				$r = false;
+			}
+		} else {
+			$r = false;
+		}
+
+		if ($r) {
+			echo '正確';
+		// $this->load->model('m_login');
+			// $this->m_login->AccountLogin($account);
+			// redirect('/layout_dashboard');
+		} else {
+			echo '錯誤';
+			// redirect('/loginpage');
+		}
+
+		// echo base_url();
+		// echo '成功';
+		// echo $r;
+		// return $r;
+
+		$this->data['title'] = '出勤狀況';
+		$this->data['content'] = 'checkin.php';
+		$this->load->view('layout_dashboard', $this->data);
+	}
+
+	public function register() {
+		$account = $this->input->post('account');
+		$password = $this->input->post('password');
+		$sql = 'INSERT INTO  users(`account`,`password`) VALUES (?,?)';
+		$q = $this->db->query($sql, [$account, md5($password)]);
+		if ($this->db->affected_rows() > 0) {
+		} else {
+		}
+		echo '成功';
+	}
+
+	public function AccountLogin($name, $password) {
+	}
+
+	public function GetSession() {
+	}
+}
---
+++ application/views/loginpage.php
@@ -0,0 +1,33 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+    <link href="css/css.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+<div align="center">
+    <form action="login/register" id = form1 method="post">
+    <!-- <form action="login/sign_in" id = form1 method="post"> -->
+    <p>Login</p>
+    <table border="1" wedth=200 align="center">
+        <tr align="center" >
+            <td>account</td>
+            <td ><input id="idaccount" type="text" name="account"/></td>
+        </tr>
+        <tr align="center">
+            <td >password</td>
+            <td ><input id="idpassword" type="password" name="password"/></td>
+        </tr>
+        <tr >
+            <td colspan="2" align="center"><button id="add_submit" name="submit">送出</button></td>
+        </tr>
+    </form>
+</div>
+</body>
+</html>
+
